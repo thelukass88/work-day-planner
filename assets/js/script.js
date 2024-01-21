f60e236e8a6967158670e476df811f095c8ab1d8
thelukass88 <l.holliday88@hotmail.com>
2024-01-21 16:11:25 +0000

    get stored tasks and html containers

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,42 @@
           start: 08,
           end: 20,
         };
+    
+    
+    // get stored tasks
+    let taskData = JSON.parse(localStorage.getItem('taskData')) || [];
+    // render blocks
+    function renderBlocks() {
+      for (let i = hours.start; i <= hours.end; i++) {
+        blocks.push(moment().hour(i).format('HH:00'));
+      }
+      let today = moment().format('LL');
+      for (let i = 0; i < blocks.length; i++) {
+        $('#container').append(
+          `<div class="row">
+              <div class="hour col-2 d-flex justify-content-left align-items-center h-100">
+                <span class="block-time" data-index="${i}">
+                  ${blocks[i]}
+                </span>
+              </div>
+              <div class="col-7 col-md-8 d-flex w-100 h-100 p-0">
+                <textarea class="daily-task w-100 h-100" data-index="${i}">${
+            taskData[i] && taskData[i].date === today ? taskData[i].task : ''
+          }</textarea>
+              </div>
+              <div class="buttons-wrapper col-3 col-md-2 d-flex p-0 w-100 h-100">
+                <button class="btn save-button d-flex align-items-center justify-content-center w-100 h-100" data-index="${i}">
+                  <i class="fa-solid fa-plus"></i>
+                </button>
+                <button class="btn clear-button d-flex align-items-center justify-content-center w-100 h-100" data-index="${i}">
+                  <i class="fa-solid fa-trash-can"></i>
+                </button>
+              </div>
+          </div>`
+        );
+      }
+    }
+    renderBlocks();
+
+    
 })(jQuery);
